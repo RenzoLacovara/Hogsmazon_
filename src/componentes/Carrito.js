69266d5c2f24d87c1b90bb42c8d6b642854ea792
Renzo Lacovara <rnlacovara@gmail.com>
2023-01-17 20:16:36 -0300

    destructed objets added

--- src/componentes/Carrito.js
+++ src/componentes/Carrito.js
@@ -4,46 +4,46 @@ import { useCarrito } from "./CustomProvider";
 
 const Carrito = (productos) => {
   const { removeItem, sumarProducto, restarProducto } = useCarrito();
-
+  const { nombre, precio, imagen, cantidad, id } = productos;
   return (
     <div className="text-center flex flex-row w-full h-52 sm:h-40 justify-evenly items-center m-auto p-5 odd:bg-secundario">
       <img
         className="w-2/4 sm:w-1/4 h-full object-contain hover:animate-wiggle"
-        src={productos.imagen}
+        src={imagen}
         alt="producto"
       />
       <div className="w-2/4 sm:w-3/4 flex sm:flex-row ">
         <div className="sm:flex w-full">
           <div className="sm:w-1/3">
             <h1 className=" font-semibold">Product</h1>
-            <h1>{productos.nombre}</h1>
+            <h1>{nombre}</h1>
           </div>
           <div className="sm:w-1/3">
             <h1 className=" font-semibold">Price</h1>
-            <h1>${productos.precio}</h1>
+            <h1>${precio}</h1>
           </div>
           <div className="sm:w-1/3">
             <h1 className=" font-semibold">Quantity</h1>
             <div className="flex justify-center gap-2">
               <button
                 className={`${
-                  productos.cantidad > 1
+                  cantidad > 1
                     ? "hover:text-detalle"
                     : "hover:text-secundariod cursor-default"
                 }`}
-                onClick={() => restarProducto(productos.id)}
+                onClick={() => restarProducto(id)}
               >
                 {" "}
                 <i class="fa-solid fa-minus"></i>{" "}
               </button>
-              <h1>{productos.cantidad}u</h1>
+              <h1>{cantidad}u</h1>
               <button
                 className={`${
-                  productos.cantidad < 10
+                  cantidad < 10
                     ? "hover:text-detalle"
                     : "hover:text-secundariod cursor-default"
                 }`}
-                onClick={() => sumarProducto(productos.id)}
+                onClick={() => sumarProducto(id)}
               >
                 {" "}
                 <i class="fa-solid fa-plus"></i>{" "}
@@ -52,10 +52,7 @@ const Carrito = (productos) => {
           </div>
         </div>
         <div className="flex justify-end items-start sm:items-center">
-          <button
-            className="hover:text-detalle"
-            onClick={() => removeItem(productos.id)}
-          >
+          <button className="hover:text-detalle" onClick={() => removeItem(id)}>
             <i class="fa-solid fa-circle-xmark"></i>
           </button>
         </div>
--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -18,9 +18,10 @@ const ItemDetail = ({ detalle }) => {
     agregarProducto(detalle, cantidad);
     setTotal(cantidad);
   };
-
-  let ofe = detalles.precio - detalles.precio * detalles.descuento;
-  return detalles.nombre === undefined ? (
+  const { precio, descuento, nombre, imagen, tienda, descripcion, oferta } =
+    detalles;
+  let ofe = precio - precio * descuento;
+  return nombre === undefined ? (
     <div className="p-4 text-xl flex justify-center mt-3">
       <Snitch />
     </div>
@@ -30,45 +31,45 @@ const ItemDetail = ({ detalle }) => {
         <div className="sm:bg-mapa lg:w-2/5 h-2/4 sm:h-2/5 lg:h-full bg-circle bg-contain bg-no-repeat bg-center sm:bg-cover mx-4 sm:m-0">
           <img
             className="h-full p-10 w-full object-contain bg-cover overflow-hidden hover:animate-wiggle sm:p-2 lg:p-8 filter drop-shadow-lg"
-            src={detalles.imagen}
-            alt={detalles.nombre}
+            src={imagen}
+            alt={nombre}
           />
         </div>
 
         <div className=" lg:w-3/5 h-2/4 sm:h-3/5 lg:h-full flex flex-col justify-between">
           <div className="text-center h-5/6">
-            <h2 className="text-xl sm:text-2xl">{detalles.nombre}</h2>
+            <h2 className="text-xl sm:text-2xl">{nombre}</h2>
             <h3 className="font-light">
               by{" "}
               <Link
-                to={"/Products/Shops/" + detalles.tienda.id}
+                to={"/Products/Shops/" + tienda.id}
                 className="text-detalle font-medium"
               >
-                {detalles.tienda.nombre}
+                {tienda.nombre}
               </Link>
             </h3>
 
             <p className="text-justify p-4 sm:p-6 text-sm font-medium">
-              {detalles.descripcion}
+              {descripcion}
             </p>
           </div>
           <div className="flex h-1/6 justify-center items-center mb-1 border-t-2 mx-16 border-secundario">
             <h3
               className={` inline-block ${
-                detalles.oferta === true
+                oferta === true
                   ? "text-sm line-through"
                   : "text-detalle font-bold text-xl sm:text-2xl"
               }`}
             >
-              ${detalles.precio}
+              ${precio}
             </h3>
-            {detalles.oferta === true && (
+            {oferta === true && (
               <>
                 <h3 className="inline-block text-detalle font-bold ml-2 text-2xl">
                   ${ofe}
                 </h3>
                 <h3 className=" ml-2 inline bg-detalle text-princ px-1 rounded-md text-md">
-                  %{detalles.descuento * 100}
+                  %{descuento * 100}
                 </h3>
               </>
             )}
