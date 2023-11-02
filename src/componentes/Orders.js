f946db10230a19e790e83e5f739b5e142b579ef0
Renzo Lacovara <rnlacovara@gmail.com>
2023-11-01 23:26:42 -0300

    order style

--- src/componentes/Orders.js
+++ src/componentes/Orders.js
@@ -27,28 +27,59 @@ const Orders = () => {
   }
 
   return (
-    <div className="m-60">
+    <div className="w-full mb-20 text-center ">
       <input
         type="text"
         placeholder="Introduce el código"
         value={codigo}
         onChange={(e) => setCodigo(e.target.value)}
+        className="my-20"
       />
       <button onClick={buscarOrden}>Buscar</button>
       {resultado.length > 0 ? (
         <div>
           {resultado.map((orden, index) => (
-            <div key={index} className="card">
-              <h3>Orden #{orden.orden}</h3>
-              <p>
-                {orden.cart.map((item, index) => (
-                  <li>
-                    {item.nombre} x {item.cantidad}u - $
-                    {item.cantidad * item.precio}
-                  </li>
-                ))}
-              </p>
-              <h2>Total:${orden.valorTotal}</h2>
+            <div key={index} className="text-center ">
+              <h3>
+                Orden <span className=" text-detalle">#{orden.orden}</span>
+              </h3>
+              <div className="mt-8 mb-8 ">
+                <section className="grid items-center justify-center w-5/6 h-full grid-cols-1 m-auto my-5 overflow-hidden shadow-lg rounded-xl sm:w-3/4 lg:w-2/4">
+                  {orden.cart.map((item, index) => (
+                    <div className="flex flex-row items-center w-full p-5 m-auto text-center h-52 sm:h-40 justify-evenly odd:bg-secundario">
+                      <img
+                        className="object-contain w-2/4 h-full sm:w-1/4 hover:animate-wiggle"
+                        src={item.imagen}
+                        alt="producto"
+                      />
+                      <div className="flex w-2/4 sm:w-3/4 sm:flex-row ">
+                        <div className="w-full sm:flex">
+                          <div className="sm:w-1/3">
+                            <h1 className="font-semibold ">Product</h1>
+                            <h1>{item.nombre}</h1>
+                          </div>
+                          <div className="sm:w-1/3">
+                            <h1 className="font-semibold ">Price</h1>
+                            <h1>${item.precio}</h1>
+                          </div>
+                          <div className="sm:w-1/3">
+                            <h1 className="font-semibold ">Quantity</h1>
+                            <div className="flex justify-center gap-2">
+                              <h1>{item.cantidad}u</h1>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="flex items-start justify-end sm:items-center"></div>
+                      </div>
+                    </div>
+                  ))}
+                </section>
+              </div>
+
+              <h2>
+                Total:
+                <span className=" text-detalle">${orden.valorTotal}</span>
+              </h2>
             </div>
           ))}
         </div>
